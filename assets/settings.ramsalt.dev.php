453b3bc3d2650e6b93b704e6fc7f48c58b069355
Stephan Zeidler <stephan@ramsalt.com>
2021-04-15 15:37:58 +0200

    Remove dev and stage settings.
    They can be manually added to a project, when needed

--- assets/settings.ramsalt.dev.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$config['system.logging']['error_level'] = 'verbose';
-
-// Uncomment and specify site folder if you use stage file proxy
-//$config['stage_file_proxy.settings']['origin'] = 'http://{site}'; // no trailing slash
-//$config['stage_file_proxy.settings']['origin_dir'] = 'sites/{site}/files';
